b226189d2ff84682a265bc4349c87c4b0691c333
jiangtao <jiangtao_wx@ysten.com>
2021-07-27 18:24:06 +0800

    Fresco读单张图片

--- app/src/main/java/com/example/myapplication/Practice1_2Activity.java
+++ app/src/main/java/com/example/myapplication/Practice1_2Activity.java
@@ -5,51 +5,30 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.io.IOException;
 
 public class Practice1_2Activity extends AppCompatActivity {
-    TextView responseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_practice11);
-        responseText = (TextView)findViewById(R.id.response_txt);
-        sendRequestWithOkhttp();
         Fresco.initialize(this);
+        setContentView(R.layout.activity_practice12);
+
+
+
+        Uri uri = Uri.parse("http://images.center.bcs.ottcn.com//images/ysten/cps/cmhlj/2021072114/cb0fc4f48f7c48f29ab4ee62f82f6392.jpg");
+        SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
+        draweeView.setImageURI(uri);
     }
 
-    private void sendRequestWithOkhttp() {
-        final String url_example = "http://cosepg.t.taipan.jsa.bcs.ottcn.com:8084/ysten-epg/epg/getPsList." +
-                "shtml?catgId=2220637&templateId=44800196&pageSize=240&pageNo=1&abilityString=" +
-                "{\"abilities\":[\"NxM\",\"timeShift\",\"4K-1|cp-TENCENT\"],\"businessGroupIds\"" +
-                ":[],\"deviceGroupIds\":[\"2072\"],\"districtCode\":\"320200\",\"labelIds\":" +
-                "[\"3251\",\"3252\",\"3253\",\"3254\",\"3259\",\"328\"],\"userGroupIds\":[\"228\"]" +
-                ",\"userLabelIds\":[\"3251\",\"3252\",\"3253\",\"3254\",\"3259\",\"328\"]}" +
-                "&serviceChannelId=\n";
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder().url(url_example).build();
-                    Log.d("测试1","成功");
-                    Response response = client.newCall(request).execute();
-                    Log.d("测试2","成功");
-                    String responseData = response.body().string();
-                    Log.d("测试3",responseData);
-                    //数据显示处理
-                } catch (IOException e) {
-                    System.out.println("出错了");
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
+
 }
