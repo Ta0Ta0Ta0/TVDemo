181f6135db8b42923742928fdc8b2071f7ee7bf0
jiangtao <jiangtao_wx@ysten.com>
2021-07-27 19:15:58 +0800

    第二项再开始

--- app/src/main/java/com/example/myapplication/Practice1_2Activity.java
+++ app/src/main/java/com/example/myapplication/Practice1_2Activity.java
@@ -24,12 +24,38 @@ public class Practice1_2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fresco.initialize(this);
         setContentView(R.layout.activity_practice12);
-
-
-
         Uri uri = Uri.parse("http://images.center.bcs.ottcn.com//images/ysten/cps/cmhlj/2021072114/cb0fc4f48f7c48f29ab4ee62f82f6392.jpg");
         SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
         draweeView.setImageURI(uri);
+        sendRequestWithOkhttp();
+    }
+    private void sendRequestWithOkhttp() {
+        final String url_example = "http://cosepg.t.taipan.jsa.bcs.ottcn.com:8084/ysten-epg/epg/getPsList." +
+                "shtml?catgId=2220637&templateId=44800196&pageSize=240&pageNo=1&abilityString=" +
+                "{\"abilities\":[\"NxM\",\"timeShift\",\"4K-1|cp-TENCENT\"],\"businessGroupIds\"" +
+                ":[],\"deviceGroupIds\":[\"2072\"],\"districtCode\":\"320200\",\"labelIds\":" +
+                "[\"3251\",\"3252\",\"3253\",\"3254\",\"3259\",\"328\"],\"userGroupIds\":[\"228\"]" +
+                ",\"userLabelIds\":[\"3251\",\"3252\",\"3253\",\"3254\",\"3259\",\"328\"]}" +
+                "&serviceChannelId=\n";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url(url_example).build();
+                    Log.d("测试1","成功");
+                    Response response = client.newCall(request).execute();
+                    Log.d("测试2","成功");
+                    String responseData = response.body().string();
+                    Log.d("测试3",responseData);
+
+                    //自定义函数用于显示数据
+                } catch (IOException e) {
+                    System.out.println("出错了");
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
 
